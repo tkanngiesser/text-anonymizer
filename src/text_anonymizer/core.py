d0e731b34e0d510dcd7c7526668def3656f559ab
tkanngiesser <tinokanngiesser@gmail.com>
2025-03-17 17:26:50 +0100

    feat: enhance recognize_entities to support company terms and names

--- src/text_anonymizer/core.py
+++ src/text_anonymizer/core.py
@@ -1,61 +1,62 @@
 import re
+from typing import List, Dict, Optional
 import spacy
+from spacy.matcher import PhraseMatcher
 from typing import List, Dict, Tuple
 
 nlp = spacy.load("en_core_web_sm")
 
-def recognize_entities(text: str) -> List[Dict]:
-    """
-    Identifies various types of entities in a given text using NER and regex for URLs.
 
-    Args:
-        text: The text to be processed.
-
-    Returns:
-        A list of dictionaries, where each dictionary represents an entity and contains the following keys:
-        - type: The type of entity (e.g., "PERSON", "ORGANIZATION", "LOCATION", "PHONE", "EMAIL", "URL", "PRODUCT").
-        - start: The starting index of the entity in the text.
-        - end: The ending index of the entity in the text.
-        - text: The original text of the entity.
-    """
+def recognize_entities(
+    text: str, company_terms: Optional[List[str]] = None, company_name=None
+) -> List[Dict]:
     doc = nlp(text)
     entities = []
-
-    # NER-based entity recognition
     for ent in doc.ents:
         if ent.label_ not in ["MONEY"]:
-            entity = {
-                "type": ent.label_,
-                "start": ent.start_char,
-                "end": ent.end_char,
-                "text": ent.text,
-            }
-            entities.append(entity)
-
+            entities.append(
+                {
+                    "type": ent.label_,
+                    "start": ent.start_char,
+                    "end": ent.end_char,
+                    "text": ent.text,
+                }
+            )
     url_pattern = re.compile(
         r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
     )
     email_pattern = re.compile(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b")
-
-    for pattern, entity_type in [
-        (url_pattern, "URL"),
-        (email_pattern, "EMAIL"),
-    ]:
+    for pattern, entity_type in [(url_pattern, "URL"), (email_pattern, "EMAIL")]:
         for match in pattern.finditer(text):
-            entity = {
-                "type": entity_type,
-                "start": match.start(),
-                "end": match.end(),
-                "text": match.group(),
-            }
-            entities.append(entity)
-
-    # Sort entities by their starting position
+            entities.append(
+                {
+                    "type": entity_type,
+                    "start": match.start(),
+                    "end": match.end(),
+                    "text": match.group(),
+                }
+            )
+    if company_terms:
+        matcher = PhraseMatcher(nlp.vocab)
+        patterns = [nlp.make_doc(term) for term in company_terms]
+        matcher.add(company_name, None, *patterns)
+        for match_id, start, end in matcher(doc):
+            span = doc[start:end]
+            entities.append(
+                {
+                    "type": "COMPANY",
+                    "start": span.start_char,
+                    "end": span.end_char,
+                    "text": span.text,
+                }
+            )
     entities.sort(key=lambda x: x["start"])
-
     return entities
 
-def anonymize(text: str) -> Tuple[str, Dict[str, str]]:
+
+def anonymize(
+    text: str, company_terms: Optional[List[str]] = None, company_name=None
+) -> Tuple[str, Dict[str, str]]:
     """
     Anonymizes the given text by replacing identified entities with placeholders.
 
@@ -65,11 +66,13 @@ def anonymize(text: str) -> Tuple[str, Dict[str, str]]:
     Returns:
         A tuple containing the anonymized text and the anonymization map.
     """
-    entities = recognize_entities(text)
+    entities = recognize_entities(
+        text=text, company_terms=company_terms, company_name=company_name
+    )
     anonymization_map = {}
     entity_counters = {}
     for entity in reversed(entities):  # Process entities from end to start
-        entity_type = entity['type']
+        entity_type = entity["type"]
         if entity_type not in entity_counters:
             entity_counters[entity_type] = 1
         else:
@@ -78,6 +81,7 @@ def anonymize(text: str) -> Tuple[str, Dict[str, str]]:
         anonymization_map[placeholder] = entity["text"]
         text = text[: entity["start"]] + placeholder + text[entity["end"] :]
     return text, anonymization_map
+
 
 def deanonymize(anonymized_text: str, anonymization_map: Dict[str, str]) -> str:
     """
